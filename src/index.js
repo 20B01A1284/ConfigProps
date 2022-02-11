73870d9013d6ffadc755f022dd41467a2333565c
Tejaswini <kolleparatejaswini978@gmail.com>
2022-02-11 18:00:12 +0530

    added inline style to indexfile changed string to num

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentTable from "./props.js";
-import './Styles.css'
 
 const App = () => {
   return (
-    <table className="ui very basic collapsing celled table">
+    <table className="ui very basic collapsing celled table" style={{paddingTop: "100px",
+      marginLeft: "500px"}}>
       <thead>
         <tr>
           <th>Employee</th>
           <th>Correct Guesses</th>
         </tr>
       </thead>
-      <CommentTable name="Lena" sub="Human Resources" guess="22" />
-      <CommentTable name="Matthew" sub="Fabric Design" guess="15" />
-      <CommentTable name="Lindsay" sub="Entertainment" guess="12" />
-      <CommentTable name="Mark" sub="Executive" guess="11" />
+      <CommentTable name="Lena" sub="Human Resources" guess={22} />
+      <CommentTable name="Matthew" sub="Fabric Design" guess={15} />
+      <CommentTable name="Lindsay" sub="Entertainment" guess={12} />
+      <CommentTable name="Mark" sub="Executive" guess={11} />
     </table>
   );
 };
